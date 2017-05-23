ed308c55cbc6516d4df27fd71db9af8e06006d50
Tarik ALAOUI M'HAMDI <alaoui@agencedudev.fr>
2017-05-23 10:13:20 +0200

    Ajout de deux tests Unitaires testCurlPost et testIsLogged

--- EngieSE/EngieSETests/EngieSETests.m
+++ EngieSE/EngieSETests/EngieSETests.m
@@ -24,16 +24,49 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+-(void) testIsLogged{
+    BOOL isLoggedIn = [[NSUserDefaults standardUserDefaults] boolForKey:@"isLoggedIn"];
+    XCTAssert(isLoggedIn == true, "User is logged In");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+-(void) testCurlPost{
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
+        
+        NSString *parameters = @"email=demo@test.com&name=DemoName&surname=DemoSurname";
+        NSData *PostData = [parameters dataUsingEncoding:NSUTF8StringEncoding];
+        
+        NSURL *url = [NSURL URLWithString:@"http://jsonplaceholder.typicode.com/users"];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+        [request setHTTPMethod:@"POST"];
+        [request setHTTPBody:PostData];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+
+        
+        
+                NSURLSession *session = [NSURLSession sharedSession];
+                [[session dataTaskWithRequest:request
+                            completionHandler:^(NSData *data,
+                                                NSURLResponse *response,
+                                                NSError *error) {
+                                
+                                XCTAssertNil(error, @"dataTaskWithURL error %@", error);
+                                
+                                long status_code = (long)[(NSHTTPURLResponse *)response statusCode];
+                                
+                                if(!error && data){
+                                    XCTAssertNil(error, "Error should be null");
+                                    XCTAssertEqual(status_code, 201, "Success response should be 201");
+                                }else{
+                                    XCTAssertNotNil(error, "Error shouldn't be null");
+                                    XCTFail("Response was not NSHTTPURLResponse");
+
+                                }
+                                dispatch_semaphore_signal(semaphore);
+                            }] resume];
+
+        
+        long rc = dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, 60.0 * NSEC_PER_SEC));
+        XCTAssertEqual(rc, 0, @"network request timed out");
+}
 @end
